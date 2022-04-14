2ead4f4738e48adf354492def0ceae5afd0bd490
hanmok <dmammmm@naver.com>
2022-04-15 01:34:49 +0900

    Initial Commit, learned Styling Basics

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World22222!</Text>
+      <Text style={{margin: 16, borderWidth: 1, borderColor: 'blue', padding: 5}}> Hi!! </Text>
+      <Text style={styles.dummyText}>Hello</Text>
+      <Button title='Tap me!'/> 
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +20,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {margin: 16, 
+      padding: 16,
+      borderWidth: 2,
+      borderColor: 'red'}
 });
